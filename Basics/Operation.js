8fa018683c447229aa60f820d5a79b4833e37925
Vishnu Bhardwaj <137619796+Vishnubhr11@users.noreply.github.com>
2023-10-11 10:40:00 +0000

    JAVASCRIPT PRACTICE

---
+++ Basics/Operation.js
@@ -0,0 +1,14 @@
+/*let value = 3;
+let negativevalue = -value;
+console.log(negativevalue);*/
+ /*let firstname= "vishnu"
+ let lastname ="bhardwaj"
+ let fullname = firstname+" "+lastname
+ console.log(fullname);*/
+ /*console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+"2");
+ console.log("1"+2+2);
+ console.log(1+"2"+"2");
+ console.log(1+2+"2");*/
+ 

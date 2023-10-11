eb31d69b76039eb5054e6178ec9062f008ff516c
Vishnu Bhardwaj <137619796+Vishnubhr11@users.noreply.github.com>
2023-10-11 08:04:53 +0000

    practice

---
+++ Basics/Datatype.js
@@ -0,0 +1,8 @@
+/*let name = "vishnu"
+let intnum = Number(name)
+console.log(intnum)
+console.log(typeof(intnum)) */
+let id = 21
+let intnum = String(id)
+console.log(intnum)
+console.log(typeof(intnum))
---
+++ Basics/Varconst.js
@@ -0,0 +1,12 @@
+const name = "vishnu"
+const id=123
+let email="vishnubhardwaj225@gmail.com"
+var accountpass="ramshyam"
+acountcity ="Patna"
+ console.log(id);
+ console.log(email);
+ console.log(accountpass);
+ console.log(name);
+ console.log(acountcity);
+ console.table([id,name,acountcity,email,accountpass])
+
